5dfa53ab0659826ffc9e3b2f0b55a8c2b4ac8bb2
Peter Helenefors <peter@hertigcarl.se>
2015-04-14 10:49:44 +0200

    successfully installs h2o

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,29 @@
 # Cookbook Name:: cookbook-h2o
 # Recipe:: default
 #
-# Copyright (C) 2015 YOUR_NAME
+# Copyright (C) 2015 Peter Helenefors
 #
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe 'apt'
+include_recipe 'git'
+#include_recipe 'cmake'
+
+package 'cmake make gcc g++ libyaml-dev libssl-dev pkg-config'
+
+git '/usr/src/h2o' do
+  repository 'https://github.com/h2o/h2o.git'
+  revision 'master'
+  action :sync
+end
+
+execute 'cmake -DCMAKE_INSTALL_PREFIX=/usr/local .' do
+	cwd '/usr/src/h2o'
+end
+execute 'make' do
+	cwd '/usr/src/h2o'
+end
+execute 'sudo make install' do
+	cwd '/usr/src/h2o'
+end
